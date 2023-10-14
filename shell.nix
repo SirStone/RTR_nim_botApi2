5d03cc0d74fd4b89685fede20c83ae175188344c
Davide Cappellini <signorsasso@gmail.com>
2023-10-14 17:16:05 +0200

    nix shell piu' bella

--- shell.nix
+++ shell.nix
@@ -1,7 +1,11 @@
-{ pkgs ? import <nixos-unstable> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = [ 
-      pkgs.buildPackages.nim2
-      pkgs.buildPackages.nimPackages.nimble
-    ];
+let 
+  unstable = import <nixos-unstable> {};
+  stable = import <nixos> {};
+in
+stable.mkShell {
+  nativeBuildInputs = [ 
+    unstable.buildPackages.nim2
+    unstable.buildPackages.nimPackages.nimble
+    stable.openjdk
+  ];
 }
