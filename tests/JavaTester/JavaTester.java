54c225fd9ac9a46a1075ae27e3a5d7dd456da96a
= <=>
2024-04-01 10:11:31 +0200

    java bot for tests and compares

---
+++ tests/JavaTester/JavaTester.java
@@ -0,0 +1,29 @@
+import dev.robocode.tankroyale.botapi.*;
+import dev.robocode.tankroyale.botapi.events.*;
+
+public class JavaTester extends Bot {
+
+    int direction = 1; // Clockwise or counterclockwise
+
+    public static void main(String[] args) {
+        new JavaTester().start();
+    }
+
+    JavaTester() {
+        super(BotInfo.fromFile("JavaTester.json"));
+    }
+
+    @Override
+    public void run() {
+        while (isRunning()) {
+            turnRight(Double.MAX_VALUE * direction);
+        }
+    }
+
+    @Override
+    public void onHitByBullet(HitByBulletEvent event) {
+        System.out.println("Ouch, I got hit by a bullet!");
+        direction = -direction;
+        turnRight(Double.MAX_VALUE * direction);
+    }
+}
---
+++ tests/JavaTester/JavaTester.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp ../../assets/RTR/sample-bots-java-0.22.2/lib/* JavaTester.java 
