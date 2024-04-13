0565435e1935923cefd3774efd0306b04ab5b8ae
Davide Cappellini <signorsasso@gmail.com>
2024-04-13 18:14:50 +0200

    launcher script for starting the editor while inside the toolbox

---
+++ edit.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# export the nim libraryes to path
+export PATH=/var/home/davide/.nimble/bin:$PATH
+
+# open Visual studio code on the root project folder
+code /var/home/davide/PROJECTS/RTR_nim_botApi2 &
